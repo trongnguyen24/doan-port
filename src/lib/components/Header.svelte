b9abab45c2788ea2f41343be73c9a4936bf718a6
trongnguyen24 <trongnguyen24@gmail.com>
2024-01-08 17:00:28 +0700

    update

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,13 @@
+<nav>
+	<div class="nav-wrapper">
+		<ul id="nav-mobile" class="right hide-on-med-and-down">
+			<li><a href="#">Navbar Link</a></li>
+			<li><a href="#">Navbar Link</a></li>
+			<li><a href="#">Navbar Link</a></li>
+			<li><a href="#">Navbar Link</a></li>
+		</ul>
+	</div>
+</nav>
+
+<style>
+</style>
