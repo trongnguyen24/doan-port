35115611c6e8e0dfc91e90291db2fe4bd351ceab
trongnguyen24 <trongnguyen24@gmail.com>
2024-01-10 08:30:23 +0700

    Update Header.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,13 +4,16 @@
 
 	onMount(() => {
 		document.getElementById('cards').onmousemove = (e) => {
-			for (const card of document.getElementsByClassName('card')) {
-				const rect = card.getBoundingClientRect(),
-					x = e.clientX - rect.left,
-					y = e.clientY - rect.top;
+			// Kiểm tra xem sự kiện có phát sinh từ chuột không
+			if (e.pointerType === 'mouse' || e.pointerType === '') {
+				for (const card of document.getElementsByClassName('card')) {
+					const rect = card.getBoundingClientRect(),
+						x = e.clientX - rect.left,
+						y = e.clientY - rect.top;
 
-				card.style.setProperty('--mouse-x', `${x}px`);
-				card.style.setProperty('--mouse-y', `${y}px`);
+					card.style.setProperty('--mouse-x', `${x}px`);
+					card.style.setProperty('--mouse-y', `${y}px`);
+				}
 			}
 		};
 	});
