b127c8cc75b42ae6ba6377c1a5926a40356fbabe
trongnguyen24 <trongnguyen24@gmail.com>
2024-01-22 11:39:21 +0700

    update

--- src/routes/portfolio/[slug]/+page.js
+++ src/routes/portfolio/[slug]/+page.js
@@ -2,14 +2,14 @@
 /** @type {import('./$types').PageLoad} */
 export const prerender = true;
 import { error } from '@sveltejs/kit';
-export async function load(url) {
+export async function load(params) {
 	try {
-		const post = await import(`../../../posts/${url.params.slug}.svelte`);
+		const post = await import(`../../../posts/${params.slug}.svelte`);
 		return {
 			content: post.default,
 			meta: post.metadata
 		};
 	} catch (e) {
-		throw error(404, `Could not find ${url.params.slug}`);
+		throw error(404, `Could not find ${params.slug}`);
 	}
 }
