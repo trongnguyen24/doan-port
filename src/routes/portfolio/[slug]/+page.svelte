0e76205192143c00bb463a6baab10857b15f3785
trongnguyen24 <trongnguyen24@gmail.com>
2024-01-23 11:36:03 +0700

    Update +page.svelte

--- src/routes/portfolio/[slug]/+page.svelte
+++ src/routes/portfolio/[slug]/+page.svelte
@@ -2,6 +2,10 @@
 	export let data: any;
 </script>
 
+<svelte:head>
+	<title>{data.title}</title>
+	<meta name={data.description} content={data.title} />
+</svelte:head>
 <div class=" pb-24 md:pb-32">
 	<svelte:component this={data.content} />
 </div>
