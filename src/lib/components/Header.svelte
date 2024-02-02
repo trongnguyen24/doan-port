e49f7e6325df3895aa623fa1b154e0d943ddc36f
trongnguyen24 <trongnguyen24@gmail.com>
2024-02-02 16:51:52 +0700

    up

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -9,6 +9,7 @@
 	import Cat from '$lib/icon/cat.svelte';
 	import { Drawer } from '$lib/components/ui/vaul-svelte/dist';
 	import * as Dialog from '$lib/components/ui/dialog';
+	import Close from './ui/vaul-svelte/dist/vaul/components/close.svelte';
 
 	let dialogOpen = false;
 
@@ -173,11 +174,10 @@
 		>
 		<button
 			on:click={handleClick}
-			on:click={() => (dialogOpen = true)}
 			class="card size-12 overflow-hidden bg-[#EEEEF1] rounded-full relative"
 			aria-label="Contact"
 		>
-			<Drawer.Root>
+			<Drawer.Root bind:open={dialogOpen}>
 				<Drawer.Trigger
 					><div
 						class="text-[#6B6B70] bg-[#EFEFF2] rounded-full inset-px absolute flex justify-center items-center li"
@@ -186,37 +186,238 @@
 						<div class="size-12 shine absolute pointer-events-none"></div>
 					</div></Drawer.Trigger
 				>
-				<Drawer.Portal class=" relative inset-0 z-50 overflow-y-auto">
-					<Drawer.Overlay class="fixed inset-0 bg-black/40" />
+				<Drawer.Portal class="catTrigger relative inset-0 z-50 overflow-y-auto">
+					<Drawer.Overlay class=" fixed inset-0 bg-white/90" />
 					<Drawer.Content
-						class="bg-zinc-100 flex flex-col rounded-t-[10px] mt-24 fixed bottom-0 left-0 right-0"
+						class="flex flex-col mt-24 sm:left-1/2 fixed sm:w-[28rem] w- inset-x-6 sm:-ml-[14rem] bottom-6 mx-auto"
 					>
-						<div class="p-4 bg-white rounded-t-[10px] flex-1">
-							<div class="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
-							<div class="max-w-md mx-auto">
-								<Drawer.Title class="font-medium mb-4">Unstyled drawer for Svelte.</Drawer.Title>
-								<p class="text-zinc-600 mb-2">
-									This component can be used as a replacement for a Dialog on mobile and tablet
-									devices.
-								</p>
-								<p class="text-zinc-600 mb-8">
-									It uses
-									<a
-										href="https://www.bits-ui.com/docs/components/dialog"
-										class="underline"
-										target="_blank"
+						<Cat />
+						<Drawer.Close>
+							<button
+								class="absolute z-10 flex h-8 w-8 top-6 right-6 items-center justify-center rounded-full bg-[#F7F8F9] text-[#949595] transition-transform focus:scale-95 focus-visible:shadow-focus-ring-button active:scale-75"
+								type="button"
+								><svg
+									width="12"
+									height="12"
+									viewBox="0 0 12 12"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+									><path
+										d="M10.4854 1.99998L2.00007 10.4853"
+										stroke="#999999"
+										stroke-width="3"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+									></path><path
+										d="M10.4854 10.4844L2.00007 1.99908"
+										stroke="#999999"
+										stroke-width="3"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+									></path></svg
+								></button
+							>
+						</Drawer.Close>
+
+						<div
+							class="bg-white shadow-2xl shadow-gray-200 border border-gray-100 rounded-3xl overflow-hidden flex-1"
+						>
+							<header class="border-b border-[#F7F7F7] flex items-center px-6 h-20">
+								<h2 class="font-medium text-xl leading-none text-[#222222]">Contact</h2>
+							</header>
+							<div class="space-y-3 p-4 text-gray-700">
+								<div class="c-button group">
+									<svg
+										class="shrink-0"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
 									>
-										Bits' Dialog primitive
-									</a>
-									under the hood and is inspired by
-									<a
-										href="https://twitter.com/devongovett/status/1674470185783402496"
-										class="underline"
-										target="_blank"
+										<path
+											d="M2 12C2 8.22876 2 6.34315 3.17157 5.17157C4.34315 4 6.22876 4 10 4H14C17.7712 4 19.6569 4 20.8284 5.17157C22 6.34315 22 8.22876 22 12C22 15.7712 22 17.6569 20.8284 18.8284C19.6569 20 17.7712 20 14 20H10C6.22876 20 4.34315 20 3.17157 18.8284C2 17.6569 2 15.7712 2 12Z"
+											stroke="currentColor"
+											stroke-width="1.5"
+										/>
+										<path
+											d="M6 8L8.1589 9.79908C9.99553 11.3296 10.9139 12.0949 12 12.0949C13.0861 12.0949 14.0045 11.3296 15.8411 9.79908L18 8"
+											stroke="currentColor"
+											stroke-width="1.5"
+											stroke-linecap="round"
+										/>
+									</svg>
+
+									npkhanhdoan@gmail.com
+
+									<div class="c-hover">
+										<div class="moveLeft shrink-0 flex justify-center items-center">
+											{#each { length: 4 } as _, i}
+												<div
+													class="text-content transition-opacity duration-300 opacity-0 group-hover:opacity-100 flex gap-8 shrink-0 px-4"
+												>
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														fill="none"
+														viewBox="0 0 24 24"
+														stroke-width="1.5"
+														stroke="currentColor"
+														class="w-6 h-6"
+													>
+														<path
+															stroke-linecap="round"
+															stroke-linejoin="round"
+															d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+														/>
+													</svg>
+													Click to copy email
+												</div>
+											{/each}
+										</div>
+									</div>
+								</div>
+								<div class="c-button group">
+									<svg
+										class="shrink-0"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
 									>
-										this tweet.
-									</a>
-								</p>
+										<path
+											d="M10.0376 5.31617L10.6866 6.4791C11.2723 7.52858 11.0372 8.90532 10.1147 9.8278C10.1147 9.8278 10.1147 9.8278 10.1147 9.8278C10.1146 9.82792 8.99588 10.9468 11.0245 12.9755C13.0525 15.0035 14.1714 13.8861 14.1722 13.8853C14.1722 13.8853 14.1722 13.8853 14.1722 13.8853C15.0947 12.9628 16.4714 12.7277 17.5209 13.3134L18.6838 13.9624C20.2686 14.8468 20.4557 17.0692 19.0628 18.4622C18.2258 19.2992 17.2004 19.9505 16.0669 19.9934C14.1588 20.0658 10.9183 19.5829 7.6677 16.3323C4.41713 13.0817 3.93421 9.84122 4.00655 7.93309C4.04952 6.7996 4.7008 5.77423 5.53781 4.93723C6.93076 3.54428 9.15317 3.73144 10.0376 5.31617Z"
+											stroke="currentColor"
+											stroke-width="1.5"
+											stroke-linecap="round"
+										/>
+									</svg>
+
+									(+84) 939 004 133
+
+									<div class="c-hover">
+										<div class="moveLeft shrink-0 flex justify-center items-center">
+											{#each { length: 4 } as _, i}
+												<div
+													class="text-content transition-opacity duration-300 opacity-0 group-hover:opacity-100 flex gap-8 shrink-0 px-4"
+												>
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														fill="none"
+														viewBox="0 0 24 24"
+														stroke-width="1.5"
+														stroke="currentColor"
+														class="w-6 h-6"
+													>
+														<path
+															stroke-linecap="round"
+															stroke-linejoin="round"
+															d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+														/>
+													</svg>
+													Click to copy phone
+												</div>
+											{/each}
+										</div>
+									</div>
+								</div>
+								<div class="c-button group">
+									<svg
+										class="shrink-0"
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										><g
+											fill="none"
+											stroke="currentColor"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											stroke-width="1.5"
+											><path
+												d="M21 8v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5"
+											/><path
+												d="M9.099 11.826c2.535 0 2.535 4.174 0 4.174H6v-4.174m3.099 0H6m3.099 0c2.535 0 2.535-3.826 0-3.826H6v3.826M15.5 11a2.5 2.5 0 0 0-2.5 2.5h5a2.5 2.5 0 0 0-2.5-2.5M13 13.5a2.5 2.5 0 0 0 2.5 2.5c.928 0 1.49-.322 1.813-.62M17 8.5h-3"
+											/></g
+										></svg
+									>
+									fujisai6124de2
+									<div class="c-hover">
+										<div class="moveLeft shrink-0 flex justify-center items-center">
+											{#each { length: 4 } as _, i}
+												<div
+													class="text-content transition-opacity duration-300 opacity-0 group-hover:opacity-100 flex gap-8 shrink-0 px-4"
+												>
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														fill="none"
+														viewBox="0 0 24 24"
+														stroke-width="1.5"
+														stroke="currentColor"
+														class="w-6 h-6"
+													>
+														<path
+															stroke-linecap="round"
+															stroke-linejoin="round"
+															d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+														/>
+													</svg>
+
+													Open in new tab
+												</div>
+											{/each}
+										</div>
+									</div>
+								</div>
+								<div class="c-button group">
+									<svg
+										class="shrink-0"
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+										><g fill="none" stroke="currentColor" stroke-width="1.5"
+											><path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												d="M12 16a4 4 0 1 0 0-8a4 4 0 0 0 0 8"
+											/><path
+												d="M3 16V8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5Z"
+											/><path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												d="m17.5 6.51l.01-.011"
+											/></g
+										></svg
+									>
+									@fjsaix612
+									<div class="c-hover">
+										<div class="moveLeft shrink-0 flex justify-center items-center">
+											{#each { length: 4 } as _, i}
+												<div
+													class="text-content transition-opacity duration-300 opacity-0 group-hover:opacity-100 flex gap-8 shrink-0 px-4"
+												>
+													<svg
+														xmlns="http://www.w3.org/2000/svg"
+														fill="none"
+														viewBox="0 0 24 24"
+														stroke-width="1.5"
+														stroke="currentColor"
+														class="w-6 h-6"
+													>
+														<path
+															stroke-linecap="round"
+															stroke-linejoin="round"
+															d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+														/>
+													</svg>
+
+													Open in new tab
+												</div>
+											{/each}
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
 					</Drawer.Content>
@@ -231,6 +432,21 @@
 </div>
 
 <style>
+	.c-button {
+		@apply bg-gray-50 transition-transform relative overflow-hidden rounded-xl flex gap-3 items-center text-lg px-4 py-3;
+	}
+	.c-hover {
+		@apply inset-x-0 inset-y-1/2 group-hover:inset-y-0 transform transition-all duration-500 overflow-hidden absolute flex items-center font-medium text-violet-200 bg-violet-950;
+	}
+	@keyframes marquee {
+		0% {
+			transform: translateY(0);
+		}
+		100% {
+			transform: translateX(-50%);
+		}
+	}
+
 	#cards {
 		opacity: 0;
 	}
@@ -330,5 +546,44 @@
 			transparent 100%
 		);
 		z-index: 1;
+	}
+
+	.horizontalFade {
+		@apply absolute inset-y-0 w-12 z-10;
+		-webkit-backdrop-filter: blur(1px);
+		backdrop-filter: blur(1px);
+	}
+	.horizontalFade[side='left'] {
+		background: linear-gradient(to right, #f3f4f6 10%, transparent);
+		left: 0;
+		-webkit-mask-image: linear-gradient(to right, #f3f4f6 25%, transparent);
+		mask-image: linear-gradient(to right, #f3f4f6 25%, transparent);
+	}
+	.horizontalFade[side='right'] {
+		background: linear-gradient(to left, #f3f4f6 10%, transparent);
+		right: 0;
+		-webkit-mask-image: linear-gradient(to right, #f3f4f6 25%, transparent);
+		mask-image: linear-gradient(to left, #f3f4f6 25%, transparent);
+	}
+	.moveLeft {
+		animation: moveLeft 6s linear infinite;
+	}
+
+	@keyframes background-pan {
+		from {
+			background-position: 0% center;
+		}
+
+		to {
+			background-position: -200% center;
+		}
+	}
+	@keyframes moveLeft {
+		from {
+			transform: translateX(0%);
+		}
+		to {
+			transform: translateX(-25%);
+		}
 	}
 </style>
--- src/lib/icon/cat.svelte
+++ src/lib/icon/cat.svelte
@@ -1,18 +1,17 @@
 <script>
 	import { onMount } from 'svelte';
+	import gsap from 'gsap';
+
 	onMount(() => {
-		document.querySelector('.bg-white.inset-0').addEventListener('mousemove', function (e) {
-			moveIris(e, 'cat', 'iris');
-			moveIris(e, 'cat', 'iris_2');
-		});
-		document.querySelector('.bg-white.bg-opacity-90').addEventListener('mousemove', function (e) {
-			moveIris(e, 'cat', 'iris');
-			moveIris(e, 'cat', 'iris_2');
+		document.querySelector('.catTrigger').addEventListener('mousemove', function (e) {
+			moveIris(e, 'heads', 'cat', 'iris', 'iris_2');
 		});
 
-		function moveIris(e, eyeId, irisId) {
+		function moveIris(e, heads, eyeId, irisId, irisId2) {
 			const eye = document.getElementById(eyeId);
 			const iris = document.getElementById(irisId);
+			const iris2 = document.getElementById(irisId2);
+			const face = document.getElementById(heads);
 			const eyeRect = eye.getBoundingClientRect();
 
 			const eyeCenterX = eyeRect.x + eyeRect.width / 2;
@@ -24,99 +23,130 @@
 			const deltaY = mouseY - eyeCenterY;
 			const angle = Math.atan2(deltaY, deltaX);
 
+			const windowWidth = window.innerWidth;
+			// Normalize mouse X position to range [-0.5, 0.5]
+			const normalizedMouseX = mouseX / windowWidth - 0.5;
+			// Map normalized X to degree range [-5, 5]
+			const headTilt = normalizedMouseX * 30; // Multiply by 10 to get a range of [-5, 5] degrees
+
 			const irisMaxMove = eyeRect.width / 7; // Max distance iris can move from center
 			const irisX = eyeCenterX + irisMaxMove * Math.cos(angle);
 			const irisY = eyeCenterY + irisMaxMove * Math.sin(angle);
 
 			// Update iris position
-			iris.setAttribute('transform', `translate(${irisX - eyeCenterX}, ${irisY - eyeCenterY})`);
+			gsap.to(iris, {
+				x: irisX - eyeCenterX,
+				y: irisY - eyeCenterY
+			});
+			gsap.to(iris2, {
+				x: irisX - eyeCenterX,
+				y: irisY - eyeCenterY
+			});
+
+			// Update head tilt
+			gsap.to(face, {
+				rotation: -headTilt,
+				transformOrigin: 'center bottom' // Adjust as needed to change the pivot point
+			});
 		}
 	});
 </script>
 
-<div class="w-24 absolute top-0 cat -translate-y-full overflow-hidden group">
+<div class="w-24 absolute right-16 top-0 cat -translate-y-full overflow-hidden group">
 	<svg
 		class=" group-hover:translate-y-1/3 transition-transform"
 		xmlns="http://www.w3.org/2000/svg"
 		fill="none"
 		viewBox="0 0 190 190"
 	>
-		<g id="cat">
-			<path
-				id="body"
-				fill="#000"
-				d="M66 176.5c-.504-13.046 8.598-40.859 29.764-40.497 21.165 0 28.506 24.363 28.506 38.497 0 13.486-2.302 15.5-28.506 15.5-21.265 0-29.26-.454-29.764-13.5Z"
-			/>
-			<path
-				id="tail"
-				fill="#000"
-				fill-rule="evenodd"
-				d="M136.5 159c1.875-1.5 5-.875 6.5 1 3.359 4.199 3.997 9.475 3.411 13.458-.579 3.937-2.843 7.21-5.786 9.417-5.785 4.339-15.076 5.024-20.625-2.375a2.501 2.501 0 0 1 4-3c3.451 4.601 9.411 4.536 13.625 1.375C139.682 177.332 140 175.5 140 173s-1.359-5.699-4-9c-1.6-2-.578-4.137.5-5Z"
-				clip-rule="evenodd"
-			/>
-			<g id="head">
+		<g id="Frame">
+			<g id="cat">
 				<path
-					id="face"
+					id="cat-body"
+					fill="#000"
+					d="M66 176.5c-.504-13.046 8.598-40.859 29.764-40.497 21.165 0 28.506 24.363 28.506 38.497 0 13.486 3.73 15.5-28.506 15.5C66 190 66.504 189.546 66 176.5Z"
+				/>
+				<path
+					id="tail"
 					fill="#000"
 					fill-rule="evenodd"
-					d="M80.642 56.163a76.62 76.62 0 0 1 13.829-1.249c5.118 0 10.1.436 14.88 1.261 4.844-8.475 21.344-18.458 26.83-15.71 7.169 3.327 8.471 18.54 7.566 31.05 10.084 8.548 16.195 19.745 16.195 32 0 41.372-29.313 48.601-65.471 48.601C58.312 152.116 29 140.87 29 110.413c0-14.433 6.583-27.65 17.358-37.563-1.096-12.738.035-28.938 7.46-32.385 5.484-2.747 21.972 7.226 26.824 15.698Z"
+					d="M136.5 159c1.875-1.5 5-.875 6.5 1 3.359 4.199 3.997 9.475 3.411 13.458-.579 3.937-2.843 7.21-5.786 9.417-5.785 4.339-15.076 5.024-20.625-2.375a2.5 2.5 0 0 1 4-3c3.451 4.601 9.411 4.536 13.625 1.375C139.682 177.332 140 175.5 140 173s-1.359-5.699-4-9c-1.6-2-.578-4.137.5-5Z"
 					clip-rule="evenodd"
 				/>
-				<path
-					id="nose"
-					fill="#fff"
-					fill-rule="evenodd"
-					d="M91 115.5a1.5 1.5 0 0 1 1.5-1.5h5a1.5 1.5 0 0 1 1.5 1.5c0 .852-1.331 2.129-2.507 3.169-.828.732-2.158.732-2.986 0-1.176-1.04-2.507-2.317-2.507-3.169Z"
-					clip-rule="evenodd"
-				/>
-				<path
-					id="fur4"
-					stroke="#000"
-					stroke-linecap="round"
-					stroke-width="2"
-					d="M39.124 116.463c-3.545-1.497-14.748-4.771-25.773.07"
-				/>
-				<path
-					id="fur3"
-					stroke="#000"
-					stroke-linecap="round"
-					stroke-width="2"
-					d="M147.594 116.619c3.544-1.497 14.747-4.771 25.773.07"
-				/>
-				<path
-					id="fur2"
-					stroke="#000"
-					stroke-linecap="round"
-					stroke-width="2"
-					d="M37.857 118.938c-4.086.816-13.75 4.683-19.714 13.624"
-				/>
-				<path
-					id="fur1"
-					stroke="#000"
-					stroke-linecap="round"
-					stroke-width="2"
-					d="M148.86 119.094c4.086.816 13.75 4.683 19.714 13.624"
-				/>
-				<g id="eyes">
-					<g id="eye_L">
-						<circle id="eye" cx="71.5" cy="89.5" r="19.5" fill="#fff" />
+				<g id="heads">
+					<g id="head">
 						<path
-							id="iris"
+							id="face"
 							fill="#000"
 							fill-rule="evenodd"
-							d="M71.825 98.6a8.775 8.775 0 1 0 0-17.55 8.775 8.775 0 0 0 0 17.55Zm-3.25-8.45a3.575 3.575 0 1 0 0-7.15 3.575 3.575 0 0 0 0 7.15Z"
+							d="M80.642 56.163a76.629 76.629 0 0 1 13.829-1.25c5.118 0 10.1.437 14.88 1.262 4.844-8.475 21.344-18.458 26.83-15.71 7.169 3.327 8.471 18.54 7.566 31.05 10.084 8.548 16.195 19.745 16.195 32 0 41.372-29.313 48.601-65.471 48.601C58.312 152.116 29 140.87 29 110.413c0-14.433 6.583-27.65 17.358-37.563-1.096-12.738.035-28.938 7.46-32.385 5.484-2.747 21.972 7.226 26.824 15.698Z"
 							clip-rule="evenodd"
 						/>
-					</g>
-					<g id="eye_R">
-						<circle id="eye_2" cx="118.5" cy="89.5" r="19.5" fill="#fff" />
 						<path
-							id="iris_2"
-							fill="#000"
+							id="nose"
+							fill="#fff"
 							fill-rule="evenodd"
-							d="M118.825 98.6a8.775 8.775 0 1 0 0-17.55 8.775 8.775 0 0 0 0 17.55Zm-3.25-8.45a3.575 3.575 0 1 0 0-7.15 3.575 3.575 0 0 0 0 7.15Z"
+							d="M91 115.5a1.502 1.502 0 0 1 1.5-1.5h5a1.501 1.501 0 0 1 1.5 1.5c0 .852-1.331 2.129-2.507 3.169-.828.732-2.158.732-2.986 0-1.176-1.04-2.507-2.317-2.507-3.169Z"
 							clip-rule="evenodd"
 						/>
+						<path
+							id="fur4"
+							stroke="#000"
+							stroke-linecap="round"
+							stroke-width="2"
+							d="M39.124 116.463c-3.545-1.497-14.748-4.771-25.773.07"
+						/>
+						<path
+							id="fur3"
+							stroke="#000"
+							stroke-linecap="round"
+							stroke-width="2"
+							d="M147.594 116.619c3.544-1.497 14.747-4.771 25.773.07"
+						/>
+						<path
+							id="fur2"
+							stroke="#000"
+							stroke-linecap="round"
+							stroke-width="2"
+							d="M37.857 118.938c-4.086.816-13.75 4.683-19.714 13.624"
+						/>
+						<path
+							id="fur1"
+							stroke="#000"
+							stroke-linecap="round"
+							stroke-width="2"
+							d="M148.86 119.094c4.086.816 13.75 4.683 19.714 13.624"
+						/>
+						<g id="eyes">
+							<g id="eye_L">
+								<path
+									id="eye"
+									fill="#fff"
+									d="M71.5 109c10.77 0 19.5-8.73 19.5-19.5S82.27 70 71.5 70 52 78.73 52 89.5 60.73 109 71.5 109Z"
+								/>
+								<path
+									id="iris"
+									fill="#000"
+									fill-rule="evenodd"
+									d="M71.775 98.55a8.775 8.775 0 1 0 0-17.55 8.775 8.775 0 0 0 0 17.55Zm-3.25-8.45a3.575 3.575 0 1 0 0-7.15 3.575 3.575 0 0 0 0 7.15Z"
+									clip-rule="evenodd"
+								/>
+							</g>
+							<g id="eye_R">
+								<path
+									id="eye_2"
+									fill="#fff"
+									d="M118.5 109c10.77 0 19.5-8.73 19.5-19.5S129.27 70 118.5 70 99 78.73 99 89.5s8.73 19.5 19.5 19.5Z"
+								/>
+								<path
+									id="iris_2"
+									fill="#000"
+									fill-rule="evenodd"
+									d="M118.775 98.55a8.777 8.777 0 0 0 6.205-14.98A8.777 8.777 0 0 0 110 89.775a8.778 8.778 0 0 0 8.775 8.775Zm-3.25-8.45a3.577 3.577 0 0 0 2.528-6.103 3.577 3.577 0 0 0-6.103 2.528 3.577 3.577 0 0 0 3.575 3.575Z"
+									clip-rule="evenodd"
+								/>
+							</g>
+						</g>
 					</g>
 				</g>
 			</g>
@@ -125,12 +155,95 @@
 </div>
 
 <style>
-	#iris,
-	#iris_2 {
-		transition: transform 0.1s linear;
+	#cat-body {
+		animation: catScaleBody 1.5s infinite;
+		transform-origin: center bottom;
+	}
+	@keyframes catScaleBody {
+		0%,
+		90%,
+		100% {
+			transform: scaleY(1.05) scaleX(1.07);
+		}
+		45%,
+		50% {
+			transform: scaleY(1) scaleX(1);
+		}
+	}
+	#tail {
+		animation: catScaleTail 1.5s infinite;
+		transform-origin: bottom;
+	}
+	@keyframes catScaleTail {
+		0%,
+		100% {
+			transform: scaleY(1) rotate(-3deg);
+		}
+		50% {
+			transform: scaleY(0.9) rotate(3deg);
+		}
+	}
+
+	#face {
+		animation: catScaleFace 1.5s infinite;
+		transform-origin: center;
+	}
+	@keyframes catScaleFace {
+		0%,
+		95%,
+		100% {
+			transform: translateY(0);
+		}
+		45%,
+		50% {
+			transform: translateY(3px);
+		}
 	}
 	#eyes {
-		transform: scaleX(0.75);
+		animation: catMoveEyes 1.5s infinite;
 		transform-origin: center;
+	}
+	@keyframes catMoveEyes {
+		0%,
+		100% {
+			transform: translateY(0) scaleX(0.75);
+		}
+		50% {
+			transform: translateY(2px) scaleX(0.75);
+		}
+	}
+
+	#fur1,
+	#fur2,
+	#fur3,
+	#fur4 {
+		animation: catFur 1.5s infinite;
+		transform-origin: center;
+	}
+
+	@keyframes catFur {
+		0%,
+		95%,
+		100% {
+			transform: translateY(0);
+		}
+		45%,
+		50% {
+			transform: translateY(3px);
+		}
+	}
+	#head {
+		animation: catHead 3s infinite;
+		transform-origin: center bottom;
+	}
+
+	@keyframes catHead {
+		0%,
+		100% {
+			transform: rotate(-3deg);
+		}
+		50% {
+			transform: rotate(3deg);
+		}
 	}
 </style>
